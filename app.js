6705b3d771cee35fc27f3bb127a9dbb37a5ae906
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-05-25 15:01:15 +0530

    form added in index.ejs

--- app.js
+++ app.js
@@ -2,11 +2,41 @@ const express = require("express")
 
 const app = express();
 
+const userModel = require("./models/user");
+const cookieParser = require("cookie-parser");
+
+
+
+app.set("view engine" , "ejs")
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
+
+
+
+// root route 
+
 app.get("/" , (req, res)=>{
 
-    res.send("hello , it's working")
+    res.render("index")
 
 })
+
+// post - create 
+
+
+app.post("/create" , function(req, res){
+
+
+
+})
+
+
+
 
 
 app.listen(5000)
--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
-const mongooose = require("mongoose")
+const mongoose = require("mongoose")
 
-mongooose.connect("mongodb:127.0.0.1:27017/AuthX");
+mongoose.connect("mongodb://127.0.0.1:27017/AuthX");
 
 
-const userSchema = mongooose.Schema({
-    username : String,
-    name : String,
-    age : Number,
-    email : String,
-    password : String
+const userSchema = mongoose.Schema({
+    username: String,
+    name: String,
+    age: Number,
+    email: String,
+    password: String
 
 })
 
 
-module.exports = mongooose.model("user" , userSchema);
+module.exports = mongoose.model("user", userSchema);
